972136d4a6edf0851e637c3db99cbaa343b88bd4
Veeramanikandan-R-1 <r.veeramanikandany216@gmail.com>
2022-10-31 19:38:21 +0530

    backup c

--- 5.EdgeLedgerWebsite/edgeledger_website/js/main.js
+++ 5.EdgeLedgerWebsite/edgeledger_website/js/main.js
@@ -1,36 +1,35 @@
 // Initialize and add the map
 function initMap() {
   // Your location
-  const loc = { lat: 42.361145, lng: -71.057083 };
+  const loc = { lat: 13.0614191, lng: 80.259537 };
   // Centered map on location
-  const map = new google.maps.Map(document.querySelector('.map'), {
+  const map = new google.maps.Map(document.querySelector(".map"), {
     zoom: 14,
-    center: loc
+    center: loc,
   });
   // The marker, positioned at location
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
 // Sticky menu background
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function () {
   if (window.scrollY > 150) {
-    document.querySelector('#navbar').style.opacity = 0.9;
+    document.querySelector("#navbar").style.opacity = 0.9;
   } else {
-    document.querySelector('#navbar').style.opacity = 1;
+    document.querySelector("#navbar").style.opacity = 1;
   }
 });
 
-
 // Smooth Scrolling
-$('#navbar a, .btn').on('click', function(event) {
-  if (this.hash !== '') {
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate(
+    $("html, body").animate(
       {
-        scrollTop: $(hash).offset().top - 100
+        scrollTop: $(hash).offset().top - 100,
       },
       800
     );
